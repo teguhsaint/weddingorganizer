2c04ac6c73d186b836b76f15bdb685ffe91a3dca
Teguh Saint <75825300+teguhsaint@users.noreply.github.com>
2022-11-16 04:21:09 +0700

    new

---
+++ pages/gallery.php
@@ -0,0 +1,15 @@
+<link href="css/lightgallery.css" rel="stylesheet" />
+
+<div id="lightgallery">
+    <a href="img/1.jpg">
+        <img src="img/1.jpg" />
+    </a>
+    <a href="img/2.jpg">
+        <img src="img/2.jpg" />
+    </a>
+</div>
+
+<script src="https://cdn.rawgit.com/sachinchoolur/lightgallery.js/master/dist/js/lightgallery.js"></script>
+<script>
+    lightGallery(document.getElementById('lightgallery'));
+</script>
