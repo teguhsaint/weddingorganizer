eedc220faeac7d43b4662f9ca14f875fd141c10e
Teguh Saint <75825300+teguhsaint@users.noreply.github.com>
2022-11-16 22:02:08 +0700

    Update gallery.php

--- pages/gallery.php
+++ pages/gallery.php
@@ -1,11 +1,71 @@
 <link href="css/lightgallery.css" rel="stylesheet" />
 
-<div id="lightgallery">
-    <a href="img/1.jpg">
-        <img src="img/1.jpg" />
+<style>
+    body {
+        background-color: darkblue !important;
+    }
+
+    .cards {
+        display: block;
+        width: 20%;
+        margin: 20px;
+       
+    }
+
+    .w-100 {
+        width: 100%;
+        height: 50%;
+        padding: 15px;
+        border: 1px solid white;
+        /* box-shadow: 0 0 15px #00000083; */
+        border-radius: 2px;
+    }
+
+    .r1 {
+        transform: rotate(3deg);
+        transition: all;
+        transform-style: preserve-3d;
+        transition-duration: 0.4s;
+    }
+
+    .r1:hover {
+        transform: rotate(0deg);
+        transition: all;
+        transform-style: preserve-3d;
+        transition-duration: 0.4s;
+
+    }
+
+    .r4 {
+        transform: rotate(-4deg);
+        transition: all;
+        transform-style: preserve-3d;
+        transition-duration: 0.4s
+    }
+
+    .r4:hover {
+        transform: rotate(0deg);
+        
+        transition: all;
+        transform-style: preserve-3d;
+        transition-duration: 0.4s
+    }
+</style>
+
+<div id="lightgallery" style="display: flex;justify-content: center">
+    <a href="img/1.jfif" class="cards">
+
+        <img src="img/1.jfif" class="w-100 r1" />
     </a>
-    <a href="img/2.jpg">
-        <img src="img/2.jpg" />
+
+    <a href="img/13.jpg" class="cards">
+        <img src="img/13.jpg" class="w-100" />
+    </a>
+    <a href="img/cover3.jpeg" class="cards">
+        <img src="img/cover3.jpeg" class="w-100 r4" />
+    </a>
+    <a href="img/2.jpg" class="cards">
+        <img src="img/2.jpg" class="w-100" />
     </a>
 </div>
 
