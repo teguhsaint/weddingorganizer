60de84ed8d1929112cd77fbf4a4d36c54f803429
zipul <azismuhammad366@gmail.com>
2022-11-19 12:04:18 +0700

    home new

--- pages/home.php
+++ pages/home.php
@@ -14,10 +14,31 @@
 <div class="card_atas">
 
 </div>
+<style>
+.center {
+  padding: 20px 0;
+  border: 10px solid green;
+  text-align: center;
+  background: blue;
+  
+
+}
+.p{
+  font-family: "Lucida Console", "Courier New", monospace;
+}
+</style>
+
 
 <div>
     <H1 style="text-align:center; font-size: 40px">Griya Rias Pengantin Modern</H1>
-    <p style="text-align:center; font-size: 20px"> &nbsp;&nbsp;&nbsp; Rahasia pesta pernikahan yang spesial dan berkesan. Rahul Wedding and Organizer. Telah dipercaya lebih dari 10 tahun dan pilihan paket hemat.
-Jadikan momen pernikahan kamu berkesan dimata tamu undangan bersama jasa make up panggilan Jakarta barat, Rahul Wedding and Organizer. Solusi all in one tanpa khawatir kantong jebol.
-Ingin pesta pernikahanmu dikenang selamanya oleh tamu undangan? Percayakan kepada Rahul Wedding and Organizer. Harga terbaik spesial Juni 2021, promo diskon 25%.</p>
+    <p style="text-align:center; font-size: 20px"> &nbsp;&nbsp;&nbsp; Rahasia pesta pernikahan yang spesial dan berkesan. Zipul Wedding and Organizer. Telah dipercaya lebih dari 10 tahun dan pilihan paket hemat.
+Jadikan momen pernikahan kamu berkesan dimata tamu undangan bersama jasa make up panggilan Jombang, Zipul Wedding and Organizer. Solusi all in one tanpa khawatir kantong jebol.
+Ingin pesta pernikahanmu dikenang selamanya oleh tamu undangan? <br> Percayakan kepada Zipul. <br> Harga terbaik spesial Juni 2021, promo diskon 25%.</p>
+</div>
+<div class="center">
+    <h1 style=" font-size: 40px; margin: 0px; color: white;">Tentang Kami</h1>
+    <p style=" font-size: 40px; margin: 0px; color: white;font-family: "Lucida Console, Courier New, monospace;"> Zipul menawarkan beberapa wedding servis untuk kebutuhan resepsi pernikahan, meliputi:
+    </p>
+ 
+
 </div>
