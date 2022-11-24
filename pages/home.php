2ffbfd2f83068e0a4cfe899c98752518decfac1e
zipul <azismuhammad366@gmail.com>
2022-11-24 12:03:53 +0700

    wedding3

--- pages/home.php
+++ pages/home.php
@@ -23,6 +23,17 @@
   
 
 }
+.nikah {
+    width: 200px;
+    height: 250px;
+    border-radius: 20px;
+    margin: 10px;
+    padding: 10px;
+}
+.wadah {
+    display: flex;
+    justify-content: center;
+}
 
 </style>
 
@@ -37,6 +48,28 @@ Ingin pesta pernikahanmu dikenang selamanya oleh tamu undangan? <br> Percayakan 
     <h1 style=" font-size: 40px; margin: 0px; text-align:center; ">Tentang Kami</h1>
     <p style=" text-align:center; font-size: 20px; margin: 10px;  "> Zipul menawarkan beberapa wedding servis untuk kebutuhan resepsi pernikahan, <br> meliputi:
     </p>
- 
+</div>
 
+<div class="wadah">
+    
+<div class="nikah">
+    <img class="nikah" src="wedding/Baju-Bedahan-Baju-Adat-pengantin-Jawa-Barat-816x1020.jpg" alt="">
+    <h1 style="text-align: center; margin:0;">Jasa Pengantin Tradisional</h1>
+
+</div>
+<div class="nikah">
+    <img class="nikah" src="wedding/FOTO_15.jpg" alt="">
+    <h1 style="text-align: center; margin:0;">Jasa Pengantin Islami</h1>
+
+</div>
+<div class="nikah">
+    <img class="nikah" src="wedding/pur-rias-modifikasi-1.jpg" alt="">
+    <h1 style="text-align: center; margin:0;">Jasa Pengantin modifikasi</h1>
+
+</div>
+<div class="nikah">
+    <img class="nikah" src="wedding/download.jpg" alt="">
+    <h1 style="text-align: center; margin:0;">Jasa Pengantin Eropa</h1>
+
+</div>
 </div>
